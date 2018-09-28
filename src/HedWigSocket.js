59066cc7e805165422ec7a757c0ba30fbae2c2ee
Willy Hong <egg358358@gmail.com>
2018-09-29 01:38:01 +0800

    Update HedWigSocket.js

--- src/HedWigSocket.js
+++ src/HedWigSocket.js
@@ -1,5 +1,8 @@
-var HedWigSocket = (function () {
-'use strict';
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+	typeof define === 'function' && define.amd ? define(factory) :
+	(global.HedWigSocket = factory());
+}(this, (function () { 'use strict';
 
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
 
@@ -286,5 +289,5 @@ var HedWigSocket = function () {
 
 return HedWigSocket;
 
-}());
-//# sourceMappingURL=bundle.js.map
+})));
+//# sourceMappingURL=bundle.min.js.map
